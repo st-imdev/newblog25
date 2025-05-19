68c4c3c884e70800af46f632b0efa92d1897ab0e
st-imdev <124273900+st-imdev@users.noreply.github.com>
2025-05-19 09:09:12 +0100

    Fix plugin hook and regex for fleeting timestamps

---
+++ _plugins/fleeting_timestamp_formatter.rb
@@ -0,0 +1,8 @@
+Jekyll::Hooks.register [:fleeting], :pre_render do |doc|
+  doc.content = doc.content.gsub(/^([0-9]{1,2}:[0-9]{2}) -\s*(.*)$/) do
+    time = Regexp.last_match(1)
+    text = Regexp.last_match(2)
+    "<p class=\"fleeting-entry\" data-time=\"#{time}\">#{text}</p>"
+  end
+end
+
