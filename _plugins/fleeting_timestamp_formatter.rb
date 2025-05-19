3f31a51bba7bb11fc6b298e4659aa52cea8bfc3b
st-imdev <124273900+st-imdev@users.noreply.github.com>
2025-05-19 21:30:36 +0100

    Enhance fleeting notes with time-based organization, categorization, navigation, and interactive features

--- _plugins/fleeting_timestamp_formatter.rb
+++ _plugins/fleeting_timestamp_formatter.rb
@@ -1,7 +1,20 @@
 Jekyll::Hooks.register [:fleeting], :pre_render do |doc|
-  doc.content = doc.content.gsub(/^([0-9]{1,2}:[0-9]{2}) -\s*(.*)$/) do
+  doc.content = doc.content.gsub(/^([0-9]{1,2}:[0-9]{2}) -\s*(.*)$/m) do
     time = Regexp.last_match(1)
     text = Regexp.last_match(2)
-    "<p class=\"fleeting-entry\" data-time=\"#{time}\">#{text}</p>"
+    
+    # Extract and process categories/tags if present
+    category = ''
+    if text =~ /#(\w+)/
+      tag_name = $1
+      category = " data-category=\"#{tag_name}\""
+    end
+    
+    # Try to determine if this is a link entry
+    is_link = (text.include?('http://') || text.include?('https://'))
+    link_class = is_link ? " link-entry" : ""
+    
+    # Create the appropriate HTML
+    "<p class=\"fleeting-entry#{link_class}\" data-time=\"#{time}\"#{category}>#{text}</p>"
   end
 end
