fdb98ae775736b1572f9a93e57c77cb1a7b5982a
st-imdev <124273900+st-imdev@users.noreply.github.com>
2025-05-19 08:41:34 +0100

    Add styling for fleeting note timestamps

---
+++ _plugins/fleeting_timestamp_formatter.rb
@@ -0,0 +1,7 @@
+Jekyll::Hooks.register :fleeting, :pre_render do |doc|
+  doc.content = doc.content.gsub(/^([0-9]{1,2}:[0-9]{2}) -\s*(.*)$/m) do
+    time = Regexp.last_match(1)
+    text = Regexp.last_match(2)
+    "<p class=\"fleeting-entry\" data-time=\"#{time}\">#{text}</p>"
+  end
+end
